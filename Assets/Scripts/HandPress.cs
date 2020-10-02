a0e6419717a9a47622e50fa85c8ee39b16186736
admin <admin@MSI.local>
2020-10-02 12:51:54 +0100

    Update Keyboard HeadHand.unity, HandPress.cs, and HeadRay.cs

--- Assets/Scripts/HandPress.cs
+++ Assets/Scripts/HandPress.cs
@@ -2,55 +2,78 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class HandPress : MonoBehaviour {
 
-
-
-	private GameObject CurrentButtonHower;
-    private GameObject LastButton;
-
-
-	private void Update() {
-
-	}
-
-    private void OnTriggerStay(Collider other)
-    {
-        if (other.GetComponent<ButtonBehavior>())
-        {
-            other.GetComponent<ButtonBehavior>().HowerOver();
-        }
-        CurrentButtonHower = other.gameObject;
-    }
-
-    private void OnTriggerExit(Collider other)
+namespace Valve.VR.InteractionSystem
+{
+    public class HandPress : MonoBehaviour
     {
 
-        if (other.GetComponent<ButtonBehavior>())
+        private GameObject CurrentButtonHower;
+        private GameObject LastButton;
+
+
+        private void Update()
         {
-            other.GetComponent<ButtonBehavior>().ButtonExit();
+
         }
 
-        CurrentButtonHower = null;
-    }
-
-    [ContextMenu("HitButton")]
-	public void HitButton() {
-
-        if (CurrentButtonHower.GetComponent<ButtonBehavior>())
+        private void OnTriggerStay(Collider other)
         {
-            LastButton = CurrentButtonHower;
-            CurrentButtonHower.GetComponent<ButtonBehavior>().OnButtonDown();
+            if (other.GetComponent<ButtonBehavior>())
+            {
+                other.GetComponent<ButtonBehavior>().HowerOver();
+            }
+            CurrentButtonHower = other.gameObject;
         }
 
-	}
-
-	public void ReliseButton() {
-        if (LastButton && LastButton.GetComponent<ButtonBehavior>())
+        private void OnTriggerExit(Collider other)
         {
-            LastButton.GetComponent<ButtonBehavior>().OnButtonUP();
-            LastButton = null;
-            
+
+            if (other.GetComponent<ButtonBehavior>())
+            {
+                other.GetComponent<ButtonBehavior>().ButtonExit();
+            }
+
+            CurrentButtonHower = null;
         }
+
+        [ContextMenu("HitButton")]
+        public void HitButton()
+        {
+
+            if (CurrentButtonHower.GetComponent<ButtonBehavior>())
+            {
+                LastButton = CurrentButtonHower;
+                CurrentButtonHower.GetComponent<ButtonBehavior>().OnButtonDown();
+                VibrateHand(10000);
+
+            }
+
+        }
+
+        public void ReliseButton()
+        {
+            if (LastButton && LastButton.GetComponent<ButtonBehavior>())
+            {
+                LastButton.GetComponent<ButtonBehavior>().OnButtonUP();
+                LastButton = null;
+
+            }
+        }
+
+        void VibrateHand(ushort vibrationInMilisecons)
+        {
+
+            Hand hand = this.GetComponentInParent<Hand>();
+            if (hand != null)
+            {
+                Debug.Log("vibrate");
+                //hand.TriggerHapticPulse(vibrationInMilisecons);
+                
+            }
+        }
+
+
     }
 }
+
