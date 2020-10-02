b0fa6d6fd24b1a5cd92d264733a3b2fea600665f
Marty <Zexyqag@gmail.com>
2020-10-02 13:57:09 +0200

    Update Keyboard Raycast.unity, AudioPlayer.cs, and 12 more files...

---
+++ Assets/Scripts/AudioPlayer.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPlayer : MonoBehaviour {
+	private AudioSource audioSource;
+	public SoundManagerScriptableObject soundManager;
+	private void Awake() {
+		audioSource = GetComponent<AudioSource>();
+	}
+
+
+}
---
+++ Assets/Scripts/SoundManagerScriptableObject.cs
@@ -0,0 +1,8 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "Data", menuName = "ScriptableObjects/SoundManagerScriptableObject", order = 1)]
+public class SoundManagerScriptableObject : ScriptableObject {
+	public AudioClip Correct, Wrong, KeyboardClick;
+}
