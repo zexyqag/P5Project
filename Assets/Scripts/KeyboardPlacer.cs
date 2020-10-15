9bc6202121363ff2c8d3377860fd8a2c00003aa2
Marty <Zexyqag@gmail.com>
2020-10-15 10:21:29 +0200

    Update GameManager.prefab, Keyboard and Writeing Area HeadHand.prefab, and 6 more files...

--- Assets/Scripts/KeyboardPlacer.cs
+++ Assets/Scripts/KeyboardPlacer.cs
@@ -2,55 +2,66 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeyboardPlacer : MonoBehaviour
-{
-    public List<GameObject> keys;
-    public float Spacing;
-    public float sice;
-    public GameObject SpaceBar, BackSpace;
-    public Vector3 startPosition;
+public class KeyboardPlacer : MonoBehaviour {
+	public List<GameObject> keys;
+	public float Spacing;
+	public float Size;
+	public GameObject SpaceBar, BackSpace;
+	public Vector3 startPosition;
+	public Transform keyboard;
 
-    private void Start()
-    {
-        sice = sice / 100;
-        Scale();
-        Place();
+	private void Start() {
+		Debug.Log("spaceing");
+		Spacing = Spacing / 1000;
+		Size = Size / 1000;
+		Scale();
+		Place();
 
-    }
+	}
 
-    void Scale()
-    {
-        foreach (GameObject gameObject in keys)
-        {
-            gameObject.transform.localScale *= sice;
-        }
+	void Scale() {
+		foreach(GameObject gameObject in keys) {
+			gameObject.SetActive(true);
+			gameObject.transform.localScale *= Size;
+		}
 
-        SpaceBar.transform.localScale *= sice;
-        BackSpace.transform.localScale *= sice;
-    }
+		SpaceBar.transform.localScale *= Size;
+		BackSpace.transform.localScale *= Size;
 
-    void Place()
-    {
-        for (int i = 0; i < 10; i++) // top row
-        {
-            Vector3 newPos = new Vector3(startPosition.x + (Spacing * i) + sice, startPosition.y, startPosition.z);
-            keys[i].transform.position = newPos;
-        }
+		SpaceBar.SetActive(true);
+		BackSpace.SetActive(true);
+	}
 
-        for (int i = 0; i < 9; i++) // middle row
-        {
-            Vector3 newPos = new Vector3(startPosition.x + (Spacing * i) + 0.05f + sice, startPosition.y - Spacing, startPosition.z);
-            keys[i + 10].transform.position = newPos;
-        }
+	void Place() {
+		for(int i = 0; i < 10; i++) // top row
+		{
+			Vector3 newPos = new Vector3(startPosition.x + ((Spacing + Size) * i), startPosition.y, startPosition.z);
+			keys[i].transform.localPosition = newPos;
+		}
 
-        for (int i = 0; i < 7; i++) // button row
-        {
-            Vector3 newPos = new Vector3(startPosition.x + (Spacing * i) + 0.1f + sice, startPosition.y - Spacing * 2, startPosition.z);
-            keys[i + 19].transform.position = newPos;
-        }
+		for(int i = 0; i < 9; i++) // middle row
+		{
+			Vector3 newPos = new Vector3(startPosition.x + ((Spacing + Size) * i) + 0.05f, startPosition.y - (Spacing + Size), startPosition.z);
+			Debug.Log(newPos);
+			keys[i + 10].transform.localPosition = newPos;
+		}
 
-        SpaceBar.transform.position = new Vector3(startPosition.x + Spacing + 0.3f + sice, startPosition.y - Spacing * 3, startPosition.z);
-        BackSpace.transform.position = new Vector3(startPosition.x + Spacing + 0.6f + sice, startPosition.y - Spacing * 3 + 0.04f, startPosition.z);
-    }
+		for(int i = 0; i < 7; i++) // button row
+		{
+			Vector3 newPos = new Vector3(startPosition.x + ((Spacing + Size) * i) + 0.1f, startPosition.y - (Spacing + Size) * 2, startPosition.z);
+			keys[i + 19].transform.localPosition = newPos;
+		}
+
+		SpaceBar.transform.localPosition = new Vector3(startPosition.x + (((Spacing + Size) * 7) + 0.1f)/2, startPosition.y - (Spacing + Size) * 3, startPosition.z);
+		BackSpace.transform.localPosition = new Vector3(startPosition.x + ((Spacing + Size) * 8) + 0.05f, startPosition.y - (Spacing + Size) * 3 + 0.050f, startPosition.z);
+	}
+
+	private void OnDrawGizmos() {
+		Vector3 pos1 = keyboard.position, pos2 = keyboard.position + (Vector3.right * (Spacing / 1000 + Size / 1000) * 9);
+		Gizmos.DrawSphere(pos1, 0.05f);
+		Gizmos.DrawSphere(pos2, 0.05f);
+		Gizmos.DrawLine(pos1, pos2);
+		//Gizmos.DrawSphere(startPosition + (Vector3.right * ((Spacing + size) * i), 0.01f);
+	}
 
 }
