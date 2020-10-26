3aa379bbe2e354978bae36656614fa33411174f8
Alarse16 <alarse16@student.aau.dk>
2020-10-26 10:39:42 +0100

    Update Keyboard Raycast.unity and GameManager.cs

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -57,7 +57,7 @@ public class GameManager : MonoBehaviour {
 	[ContextMenu("switch imput type")]
 	public void switchInputType()
     {
-		Destroy(currentPlayerVariant);
+		Destroy(currentPlayerVariant.gameObject);
 		currentPlayerVariant = Instantiate(PlayerVariants[1]);
 	}
 	/*
