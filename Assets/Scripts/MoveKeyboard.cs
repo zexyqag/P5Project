9f036ff4a22b125e3c13f6729139a5e4d1d4d204
Alarse16 <alarse16@student.aau.dk>
2020-10-28 09:33:11 +0100

    Update Keyboard TestMove.unity, MoveKeyboard.cs, and MoveKeyboard.cs.meta

---
+++ Assets/Scripts/MoveKeyboard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveKeyboard : MonoBehaviour
+{
+    public float speed;
+    public bool bUP;
+    public GameObject Keyboard;
+
+    void Move()
+    {
+        if (bUP)
+        {
+            Keyboard.transform.Translate(new Vector3(0, speed * Time.deltaTime, 0));
+        }
+        else
+        {
+            Keyboard.transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
+        }
+          
+    }
+
+}
