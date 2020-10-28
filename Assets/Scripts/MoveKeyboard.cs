3b20a386c8be6c7ff5412ab1facafe55afe05e02
Alarse16 <alarse16@student.aau.dk>
2020-10-28 10:26:41 +0100

    Update Keyboard TestMove.unity and MoveKeyboard.cs

--- Assets/Scripts/MoveKeyboard.cs
+++ Assets/Scripts/MoveKeyboard.cs
@@ -6,7 +6,32 @@ public class MoveKeyboard : MonoBehaviour
 {
     public float speed;
     public bool bUP;
+    public bool bRotate;
     public GameObject Keyboard;
+
+    private void Start()
+    {
+        EventSystem.onButtonPressed += Deactivate;
+        EventSystem.onSwtichInputMethod += Activate;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        switch (bRotate)
+        {
+            case true:
+                if (other.CompareTag("UpAndDown")){
+                    Rotate();
+                }
+                break;
+
+            case false:
+                if (other.CompareTag("UpAndDown")){
+                    Move();
+                }
+                break;
+        }
+    }
 
     void Move()
     {
@@ -18,15 +43,29 @@ public class MoveKeyboard : MonoBehaviour
         {
             Keyboard.transform.Translate(new Vector3(0, -speed * Time.deltaTime, 0));
         }
-          
+
     }
 
-    private void OnTriggerStay(Collider other)
+    void Rotate()
     {
-        if (other.CompareTag("UpAndDown")
+        if (bUP)
         {
-            Move();
+            Keyboard.transform.Rotate(new Vector3(speed * Time.deltaTime, 0, 0));
         }
+        else
+        {
+            Keyboard.transform.Rotate(new Vector3(-speed * Time.deltaTime, 0, 0));
+        }
+    }
+
+    void Deactivate(char notInUse)
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    void Activate()
+    {
+        this.gameObject.SetActive(true);
     }
 
 }
