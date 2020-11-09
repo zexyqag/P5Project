7e081a6d6ba7561c0b3593eb65ad8516e5b31a4f
Alarse16 <alarse16@student.aau.dk>
2020-11-09 14:21:48 +0100

    Update AudioPlayer.cs, AudioPlayer.cs.meta, and 68 more files...

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    #region Public fields
+
+	public List<Transform> playerVariants;		// A list for storing the two different player variants (HeadHand && RayCast)
+	public List<Vector2> keyboards;				// A list for storing the two different keyboard size perameters (x = Size, y = Spacing)
+
+	public bool StartWithHeadHand;				// Boolean used for determaning if the test starts with HeadHand
+
+	#endregion
+
+	#region Private fields
+	private Transform currentPlayerVariant;																							// The player veriant curently existing in the scene
+
+	private List<KeyValuePair<Transform, Vector2>> PlayerAndKeyboardVariantPairs = new List<KeyValuePair<Transform, Vector2>>();	// A list of keyvalue pairs, storing the correct keyboard for a specific player variant
+
+	private bool isBothPlayerVeriantsTestet = false;                                                                                // bool is true if both player imput types has bin testet
+    #endregion
+
+    #region Unity Functions
+    private void Start() 
+	{		
+		EventSystem.onSwtichInputMethod += SwitchPlayerVariants;																// Subscripes to the switch input Event
+
+		for (int i = 0; i < playerVariants.Count; i++)																	// Iterates for each player variant (2)
+		{
+			PlayerAndKeyboardVariantPairs.Add(new KeyValuePair<Transform, Vector2>(playerVariants[i], keyboards[i]));	// Adds a key value pair with matching variant and keyboard to the key value list
+		}
+
+		SetupPlayerAndKeyboard(PlayerPrefs.GetInt("StartWithHeadHand"));												// Player prefs is used for saving data (a boolean in this case) outside of runtime
+		ReversePlayerPrefBoolean();																						// Switches the state of the player pref int, thier by changeching the nex player variant
+	}
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))	// If R is pressed on the keyboard
+        {
+			ReversePlayerPrefBoolean();		// Switch the next player variant
+			SceneManager.LoadScene(0);		// Load the start scene
+        }
+    }
+	#endregion
+
+	void ReversePlayerPrefBoolean()
+    {
+		PlayerPrefs.SetInt("StartWithHeadHand", PlayerPrefs.GetInt("StartWithHeadHand") == 1 ? 0 : 1); // switches the state of StartWithHeadHand between 1 and 0
+	}
+
+	public void SwitchPlayerVariants()
+    {
+        if (isBothPlayerVeriantsTestet)										// If both player variants are testet, retun and do nothing else in this function
+			return;
+
+		Destroy(currentPlayerVariant.gameObject);							// Destroys the current, and at this point, completet player variant
+		SetupPlayerAndKeyboard(PlayerPrefs.GetInt("StartWithHeadHand"));	// Call the next setup based on witch playervariant is next
+		isBothPlayerVeriantsTestet = true;									
+	}
+
+	private void SetupPlayerAndKeyboard(int element)
+	{ // The discreat parameter integer of this function represents the Player Variant type (HeadHand or Raycast)                                                                                  
+
+		EventSystem.onTestType(PlayerAndKeyboardVariantPairs[element].Key.name);		// Invokes the event onTestType, witch takes a string, in this case the name of the Player Variant type
+
+		currentPlayerVariant = Instantiate(PlayerAndKeyboardVariantPairs[element].Key); // Instansiate the correct player variant prefap and sets currentPlayerVariant to this variant
+
+		KeyboardPlacer KP = this.GetComponent<KeyboardPlacer>();						// Gets the Keyboard placer componant (Script) and defines it as KP for ease of reference
+		KP.Size = PlayerAndKeyboardVariantPairs[element].Value.x;						// Sets the Size of KP to the corect float, defined in the Editor
+		KP.Spacing = PlayerAndKeyboardVariantPairs[element].Value.y;                    // Sets the Spacinng of KP to the corect float, defined in the Editor
+		KP.Place();																		// Calls the place unction in KP
+	}
+}
---
+++ Assets/Scripts/StringListFromFileHandler.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class StringListFromFileHandler : MonoBehaviour {
+
+	#region Private var
+	[SerializeField] private TextAsset phrasesToWriteAsset = null;
+	private List<string> phrasesToWriteStringList = new List<string>();
+	private int phraseIndex = 0;
+	private Text textField = null;
+	private string currentString = string.Empty;
+	#endregion
+
+	private void Awake() {
+		EventSystem.onNextString += nextText;
+		if(phrasesToWriteAsset) {
+			using(System.IO.StringReader reader = new System.IO.StringReader(phrasesToWriteAsset.text)) {
+				string line;
+				while((line = reader.ReadLine()) != null) {
+					phrasesToWriteStringList.Add(line);
+				}
+			}
+		} else {
+			Debug.LogError("No text file assigned to: " + this + " on " + gameObject.name);
+			phrasesToWriteStringList = new List<string> { "No text file assigned", "You forgot to assgin a text file", "Missing text file", "Text file be gone" };
+		}
+	}
+
+	//Get nessesary refrence to game componets and initialize the string list
+	private void Start() {
+		textField = GetComponent<Text>();
+		Initialize();
+	}
+
+	/// <summary>
+	/// Shuffles the list, resets the index to zero, and calls nextText()
+	/// </summary>
+	private void Initialize() {
+		phraseIndex = 0;
+		phrasesToWriteStringList.Shuffle();
+		nextText();
+	}
+
+	/// <summary>
+	/// Updates the current string to match with the next string from the string list, index exceeds string list length Initialize is called before trying again.
+	/// </summary>
+	[ContextMenu("nextText()")]
+	public void nextText() {
+		if(phraseIndex >= phrasesToWriteStringList.Count) {
+			Initialize();
+		} else {
+			currentString = phrasesToWriteStringList.ElementAt(phraseIndex).ToUpper();
+			++phraseIndex;
+		}
+		textField.text = currentString;
+		EventSystem.onUpdateStringToMatch(currentString);
+	}
+
+	private void OnApplicationQuit() => Unsubscribe();
+	private void OnDisable() => Unsubscribe();
+	private void OnDestroy() => Unsubscribe();
+
+	/// <summary>
+	/// Unsubscribes the methods in this script from the EventSystem
+	/// </summary>
+	private void Unsubscribe() {
+		EventSystem.onNextString -= nextText;
+	}
+}
